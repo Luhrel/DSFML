ab0e82cdb986416b94dec5ceb84ded378f5e0108
Luhrel <lucien.perregaux@protonmail.ch>
2020-01-05 18:16:23 +0100

    Remove InputSoundFile and OutputSoundFile classes.
    Removed because interfacing to C++ is a bit tricky.

--- src/dsfml/audio/package.d
+++ src/dsfml/audio/package.d
@@ -2,10 +2,10 @@ module dsfml.audio;
 
 public
 {
-    import dsfml.audio.inputsoundfile;
+    //import dsfml.audio.inputsoundfile;
     import dsfml.audio.listener;
     import dsfml.audio.music;
-    import dsfml.audio.outputsoundfile;
+    //import dsfml.audio.outputsoundfile;
     import dsfml.audio.sound;
     import dsfml.audio.soundbuffer;
     import dsfml.audio.soundbufferrecorder;
